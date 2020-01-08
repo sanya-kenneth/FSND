b7c7ba5a3fe7bf004e08928c3a99ad7be0b213c7
sanya-kenneth <sanyakenneth@gmail.com>
2020-01-08 18:20:05 +0300

    Add coffeeshop backend

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -24,9 +24,16 @@ CORS(app)
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks} 
+    where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['GET'])
+@requires_auth(permission='get all drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+    drinks = [drink.short_rep() for drink in drinks]
+    return jsonify({"success": True, "drinks": drinks})
 
 
 '''
@@ -34,9 +41,17 @@ CORS(app)
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks} 
+    where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks-detail/<drink_id>', methods=['GET'])
+@requires_auth(permission='drink detail')
+def get_drink(drink_id):
+    drink = Drink.query.filter_by(id=drink_id).first()
+    if not drink:
+        abort(404)
+    return jsonify({"success": True, "drink": drink.long_rep()})
 
 
 '''
@@ -45,9 +60,27 @@ CORS(app)
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+    returns status code 200 and json {"success": True, "drinks": drink} 
+    where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks', methods=['POST'])
+@requires_auth(permission='add drink')
+def add_drink():
+    data = request.get_json()
+    title = data.get('title')
+    recipe = data.get('recipe')
+    if not isinstance(recipe, list):
+        return jsonify({"success": False,
+                        "error": 
+                            "Recipe field must contain a list of recipe objects e.g [{'color': 'blue'}]"}), 400
+    drink = Drink(title=title, recipe=recipe)
+    check_drink = Drink.query.filter_by(title=drink.title).first()
+    if check_drink:
+        return jsonify({"success": False,
+                        "error": f"Drink with title {drink.title} already exists"}), 400
+    drink.insert()
+    return jsonify({"success": True, "drink": drink.long_rep()}), 201
 
 
 '''
@@ -58,9 +91,30 @@ CORS(app)
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+    returns status code 200 and json {"success": True, "drinks": drink} 
+    where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<drink_id>', methods=["PATCH"])
+@requires_auth(permission='edit drink')
+def update_drink(drink_id):
+    data = request.get_json()
+    title = data.get("title")
+    recipe = data.get("recipe")
+    drink = Drink.query.filter_by(id=drink_id).first()
+    if not drink:
+        abort(404)
+    print(title)
+    if title:
+        setattr(drink, "title", title)
+        drink.update()
+    if recipe:
+        setattr(drink, "recipe", recipe)
+        drink.update()
+    return jsonify({"success": True,
+                    "drink": drink.long_rep()
+                    }), 200
+    
 
 
 '''
@@ -70,9 +124,18 @@ CORS(app)
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+    returns status code 200 and json {"success": True, "delete": id} 
+    where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+@app.route('/drinks/<drink_id>', methods=['DELETE'])
+@requires_auth(permission='delete drink')
+def delete_drink(drink_id):
+    drink =  Drink.query.filter_by(id=drink_id).first()
+    if not drink:
+        abort(404)
+    drink.delete()
+    return jsonify({"success": True, "delete": drink.id}), 200
 
 
 ## Error Handling
@@ -102,9 +165,22 @@ def unprocessable(error):
 @TODO implement error handler for 404
     error handler should conform to general task above 
 '''
+@app.errorhandler(404)
+def unprocessable(error):
+    return jsonify({
+                    "success": False, 
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
 
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above 
 '''
+@app.errorhandler(500)
+def autherror(error):
+    if isinstance(error, AuthError):
+        return jsonify({"success": False,
+                    "error": error.status_code,
+                    "message": error.error}), error.status_code
--- projects/03_coffee_shop_full_stack/starter_code/backend/src/database/models.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/database/models.py
@@ -35,20 +35,19 @@ a persistent drink entity, extends the base SQLAlchemy Model
 '''
 class Drink(db.Model):
     # Autoincrementing, unique primary key
-    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+    id = db.Column(db.Integer().with_variant(Integer, "sqlite"), primary_key=True)
     # String Title
-    title = Column(String(80), unique=True)
+    title = db.Column(db.String(80), unique=True)
     # the ingredients blob - this stores a lazy json blob
     # the required datatype is [{'color': string, 'name':string, 'parts':number}]
-    recipe =  Column(String(180), nullable=False)
+    recipe =  db.Column(db.PickleType(), nullable=False)
 
     '''
     short()
         short form representation of the Drink model
     '''
-    def short(self):
-        print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+    def short_rep(self):
+        short_recipe = [{"color": r['color'], "parts": r['parts']} for r in self.recipe]
         return {
             'id': self.id,
             'title': self.title,
@@ -59,11 +58,11 @@ class Drink(db.Model):
     long()
         long form representation of the Drink model
     '''
-    def long(self):
+    def long_rep(self):
         return {
             'id': self.id,
             'title': self.title,
-            'recipe': json.loads(self.recipe)
+            'recipe': self.recipe
         }
 
     '''
@@ -104,4 +103,4 @@ class Drink(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return json.dumps(self.short())
+        return json.dumps(self.short_rep())
