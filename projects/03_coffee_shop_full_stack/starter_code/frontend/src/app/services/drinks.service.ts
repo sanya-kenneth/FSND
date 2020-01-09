d03bef5bc547bb3405a903832b5e412d42f256c4
sanya-kenneth <sanyakenneth@gmail.com>
2020-01-09 03:42:53 +0300

    add coffe-shop frontend logic

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/app/services/drinks.service.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/app/services/drinks.service.ts
@@ -91,7 +91,7 @@ export class DrinksService {
   }
 
   getDrinks() {
-    if (this.auth.can('get:drinks-detail')) {
+    if (this.auth.can('drink detail')) {
       this.http.get(this.url + '/drinks-detail', this.getHeaders())
       .subscribe((res: any) => {
         this.drinksToItems(res.drinks);
