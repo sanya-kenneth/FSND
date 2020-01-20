ce92aef07234c0728386b64d7a5328298398ad67
sanya-kenneth <sanyakenneth@gmail.com>
2020-01-20 19:20:04 +0300

    ft(capstone): - Add models for capstone project - Add controllers for capstone project - Add views for capstone project - Add tests for capstone project - Restructure capstone project - Update requrements.txt file for capstone project

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -1,16 +1,22 @@
 import os
-from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from views import app_bp
+from config import app_config
+from models import db
+from auth import AuthError
 
-def create_app(test_config=None):
+
+def create_app(environment):
   # create and configure the app
   app = Flask(__name__)
+  app.config.from_object(app_config[environment])
+  db.init_app(app)
+  # Create database tables
+  # db.drop_all(app=app)
+  db.create_all(app=app)
   CORS(app)
+  app.register_blueprint(app_bp)
 
   return app
-
-APP = create_app()
-
-if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
--- projects/capstone/starter/test_app.py
+++ projects/capstone/starter/test_app.py
@@ -1,1 +1,68 @@
+import os
+import unittest
+import json
+from models import db
+from app import create_app
 
+
+class CapstoneTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.app = create_app('Testing')
+        db.create_all(app=self.app)
+        self.client = self.app.test_client()
+        self.teacher_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1qRXlRVUpDUVRsQ09UQkVNVEZDTWtWR01VWXdPVGRETlVVMk5UQTRRMFZGTlVNMVJqQkROdyJ9.eyJpc3MiOiJodHRwczovL2Rldi16NTdxMGcyYS5hdXRoMC5jb20vIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMDkwMzcyMjc2Mzg5NTg3MTMzMTEiLCJhdWQiOlsiaHR0cDovLzAuMC4wLjA6ODA4MC8iLCJodHRwczovL2Rldi16NTdxMGcyYS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNTc5Njg4MDEzLCJleHAiOjE1Nzk2OTUyMTMsImF6cCI6IjJnNzc3b2txSlg0QVRucjZvclNKc2NQdmpyU2RPRlpJIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsInBlcm1pc3Npb25zIjpbImFkZDpxdWVzdGlvbiIsImRlbGV0ZTpxdWVzdGlvbiIsImVkaXQ6cXVlc3Rpb24iLCJyZWFkOmFuc3dlciIsInJlYWQ6YW5zd2VycyIsInJlYWQ6cXVlc3Rpb24iLCJyZWFkOnF1ZXN0aW9ucyJdfQ.RNX2KgmhalPcZApU5e8ht7gQalArtdU4hT694fdn6UTXFyatHEkkHCVx2Pz6THMSm2TnCwxJkkLC_RKmg91-zvUmqkijIVZsPOCZQiDkRbhNh9whOBkPYdM8ebIGMudZn_yfZCjkHxRyW7FrIT1bp9tjTTkusTqnzBDNvjC4BLV8yko1y8ka36Ubf-bGLXLtkqORjfU2nQP6nW_ncbMvpinkSANKFIqMvTZjXYpPEUgCZoUXkjkrlbmP6vvj2vpegGtFsttYsY3IZZz9KlWox1S4DEDye6lMcNwyoVcQf24koSWv-evI2Lk44D5x_X2fh_k41kZs4jcR8MhDq9Vi2Q'
+        self.question = {
+                'question': 'What is python?',
+                'teacher_id': '12'
+            }
+
+
+    def tearDown(self):
+        db.drop_all(app=self.app)
+        
+    
+    def test_api_can_add_question(self):
+        response = self.client.post('/questions',
+                                      json=self.question,
+                                      headers={"Authorization":"Bearer {}".format(self.teacher_token)})
+        self.assertEqual(response.status_code, 201)
+
+
+    def test_api_can_get_questions(self):
+        self.client.post('/questions',
+                            json=self.question,
+                            headers={"Authorization":"Bearer {}".format(self.teacher_token)})
+        response = self.client.get('/questions',
+                                   headers={"Authorization": "Bearer {}".format(self.teacher_token)})
+        data = json.loads(response.data)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["data"][0]["question"], "What is python?")
+        
+    
+    def test_api_gets_one_question(self):
+        self.client.post('/questions',
+                            json=self.question,
+                            headers={"Authorization":"Bearer {}".format(self.teacher_token)})
+        response = self.client.get('/questions/1',
+                                   headers={"Authorization": "Bearer {}".format(self.teacher_token)})
+        data = json.loads(response.data)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["data"]["question"], "What is python?")
+
+
+    def test_api_can_delete_question(self):
+        headers = {"Authorization":"Bearer {}".format(self.teacher_token)}
+        self.client.post('/questions', json=self.question, headers=headers)
+        r = self.client.delete('/questions/1', headers=headers)
+        data = json.loads(r.data)
+        self.assertTrue(data["success"])
+        self.assertEqual(data["message"], "Question has been deleted successfuly")
+        
+        
+    def test_returns_error_if_teacher_tries_to_add_an_answer(self):
+        headers = {"Authorization":"Bearer {}".format(self.teacher_token)}
+        r = self.client.post('/question/1/answers', headers=headers)
+        data = json.loads(r.data)
+        print(data)
+        self.assertEqual(data["error"], "Permission not found.")
